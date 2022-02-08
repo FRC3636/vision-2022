354ed93e84e0bf93e45857cc353801c3bbe700e0
gagnonsilas <gagnon.silas@gmail.com>
2022-02-08 09:46:56 -0800

    feat(vision) add basic camera capture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::{
+	highgui,
+	prelude::*,
+	Result,
+	videoio,
+};
+
+fn main() -> Result<()> {
+	let window = "video capture";
+	highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
+	#[cfg(ocvrs_opencv_branch_32)]
+	let mut cam = videoio::VideoCapture::new_default()?; // 0 is the default camera
+	#[cfg(not(ocvrs_opencv_branch_32))]
+	let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 is the default camera
+	let opened = videoio::VideoCapture::is_opened(&cam)?;
+	if !opened {
+		panic!("Unable to open default camera!");
+	}
+	loop {
+		let mut frame = Mat::default();
+		cam.read(&mut frame)?;
+		if frame.size()?.width > 0 {
+			highgui::imshow(window, &mut frame)?;
+		}
+		let key = highgui::wait_key(10)?;
+		if key > 0 && key != 255 {
+			break;
+		}
+	}
+	Ok(())
 }
